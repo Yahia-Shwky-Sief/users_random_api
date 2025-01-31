57cf2a5c7981e946bf2ea63f37e69cc85b0e85d3
YAHIA-SHWKY-SIEF <yahiasief4@gmail.com>
2025-01-31 19:30:21 +0200

    init

---
+++ logic/get_data.js
@@ -0,0 +1,24 @@
+async function getData() {
+    randomNumber = Math.floor(Math.random() * 800 + 1);
+    const url = `https://randomuser.me/api/`;
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+      
+      const json = await response.json();
+      console.log(json)
+      buildWidget(json);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  function buildWidget(json) {
+    document.getElementById('img').src = json.results[0].picture.large;
+    document.getElementById('email').textContent = json.results[0].email;
+    document.getElementById('name').textContent = `${json.results[0].name.title}. ${json.results[0].name.first} ${json.results[0].name.last}`;
+  }
+
+  getData();
